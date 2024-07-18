b214dff17256342e39e613376aea3254561b7a03
jacojean01 <jeancarlos10171@gmail.com>
2024-07-17 23:23:17 -0500

    fix seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,21 +14,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Setting::create([
-            'about_title' => 'Software Developer',
-            'about_description' => 'Junior programmer focused on web programming.',
-            'fb_url' => 'https://www.instagram.com/jaco_jean_ewe/',
-            'github_url' => 'https://github.com/JacoJean07',
-            'linkedin_url' => 'https://www.linkedin.com/in/jean-carlos-cede%C3%B1o-0617732a5/',
-            'freelance_url' => '#li',
-            'cv_url' => 'https://drive.google.com/file/d/12V32bCeKg15ksxgQEjonaJ3dzZhZnbBV/view',
-            'video_url' => '#video'
-        ]);
-        // \App\Models\User::factory(1)->create();
+        // si no existen datos en la tabla settings, crear
+        if (\App\Models\Setting::count() == 0) {
+            \App\Models\Setting::create([
+                'about_title' => 'Software Developer',
+                'about_description' => 'Junior programmer focused on web programming.',
+                'fb_url' => 'https://www.instagram.com/jaco_jean_ewe/',
+                'github_url' => 'https://github.com/JacoJean07',
+                'linkedin_url' => 'https://www.linkedin.com/in/jean-carlos-cede%C3%B1o-0617732a5/',
+                'freelance_url' => '#li',
+                'cv_url' => 'https://drive.google.com/file/d/12V32bCeKg15ksxgQEjonaJ3dzZhZnbBV/view',
+                'video_url' => '#video'
+            ]);
+        }
 
-        \App\Models\User::factory()->create([
-            'name' => 'Jean Cedeño',
-            'email' => 'cmjeancarlos4@gmail.com',
-        ]);
+        // si no existen datos en la tabla users, crear
+        if (\App\Models\User::count() == 0) {
+            \App\Models\User::factory()->create([
+                'name' => 'Jean Cedeño',
+                'email' => 'cmjeancarlos4@gmail.com',
+            ]);
+        }
     }
 }
