24bef073e9aca56077ec1f98a879835fce8073a0
jacojean01 <jeancarlos10171@gmail.com>
2024-07-17 23:40:25 -0500

    fix links of head

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Favicon -->
-    <link href="{{ asset('import/assets/img/favicon.ico') }}" rel="icon">
+    <link href="import/assets/img/favicon.ico" rel="icon">
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
@@ -21,11 +21,11 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="{{ asset('import/assets/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('import/assets/lib/lightbox/css/lightbox.min.css') }}" rel="stylesheet">
+    <link href="import/assets/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="import/assets/lib/lightbox/css/lightbox.min.css" rel="stylesheet">
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="{{ asset('import/assets/css/style.css') }}" rel="stylesheet">
+    <link href="import/assets/css/style.css" rel="stylesheet">
 </head>
 
 <body data-spy="scroll" data-target=".navbar" data-offset="51">
@@ -57,19 +57,19 @@
        <!-- JavaScript Libraries -->
        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-       <script src="{{ asset('import/assets/lib/typed/typed.min.js') }}"></script>
-       <script src="{{ asset('import/assets/lib/easing/easing.min.js') }}"></script>
-       <script src="{{ asset('import/assets/lib/waypoints/waypoints.min.js') }}"></script>
-       <script src="{{ asset('import/assets/lib/owlcarousel/owl.carousel.min.js') }}"></script>
-       <script src="{{ asset('import/assets/lib/isotope/isotope.pkgd.min.js') }}"></script>
-       <script src="{{ asset('import/assets/lib/lightbox/js/lightbox.min.js') }}"></script>
+       <script src="import/assets/lib/typed/typed.min.js"></script>
+       <script src="import/assets/lib/easing/easing.min.js"></script>
+       <script src="import/assets/lib/waypoints/waypoints.min.js"></script>
+       <script src="import/assets/lib/owlcarousel/owl.carousel.min.js"></script>
+       <script src="import/assets/lib/isotope/isotope.pkgd.min.js"></script>
+       <script src="import/assets/lib/lightbox/js/lightbox.min.js"></script>
 
        <!-- Contact Javascript File -->
-       <script src="{{ asset('mail/jqBootstrapValidation.min.js') }}"></script>
-       <script src="{{ asset('import/assets/mail/contact.js') }}"></script>
+       <script src="mail/jqBootstrapValidation.min.js"></script>
+       <script src="import/assets/mail/contact.js"></script>
 
        <!-- Template Javascript -->
-       <script src="{{ asset('import/assets/js/main.js') }}"></script>
+       <script src="import/assets/js/main.js"></script>
    </body>
 
    </html>
