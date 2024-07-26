6890d94e9a552a0ba9cf76b60924b96fb83f7460
Jaco Jeanuwu <you@example.com>
2024-07-26 12:03:24 -0500

    fix images for home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -217,14 +217,16 @@
       <div class="row portfolio-container">
         @foreach ($portfolios as $portfolio)
           <div class="col-lg-12 col-md-12 mb-4 portfolio-item {{ $portfolio->category->name }}">
-            <div class="position-relative overflow-hidden" style="width: 100%;">
+            <div class="position-relative overflow-hidden">
               <h2>{{ $portfolio->title }}</h2>
-              <iframe src="{{ $portfolio->project_url }}" width="100%" height="500" frameborder="0"
-                allowfullscreen></iframe>
-              <a target="_blank" href="{{ $portfolio->project_url }}"><button class="btn btn-primary btn-sm">View</button></a>
+              <img src="{{ $portfolio->image }}" class="img-fluid border-gray-200 shadow-sm" alt="image">
+              <a target="_blank" href="{{ $portfolio->project_url }}">
+                <button class="btn btn-primary btn-sm">View</button>
+              </a>
             </div>
           </div>
         @endforeach
+
       </div>
 
     </div>
@@ -260,66 +262,66 @@
   <!-- Testimonial End -->
 
   <!-- Contact Start
-          <div class="container-fluid py-5" id="contact">
-              <div class="container">
-                  <div class="position-relative d-flex align-items-center justify-content-center">
-                      <h1 class="display-1 text-uppercase text-white" style="-webkit-text-stroke: 1px #dee2e6;">Contact</h1>
-                      <h1 class="position-absolute text-uppercase text-secondary">Contact Me</h1>
-                  </div>
-                  <div class="row justify-content-center">
-                      <div class="col-lg-8">
-                          <div class="contact-form text-center">
-                              @if (Session::has('message'))
+            <div class="container-fluid py-5" id="contact">
+                <div class="container">
+                    <div class="position-relative d-flex align-items-center justify-content-center">
+                        <h1 class="display-1 text-uppercase text-white" style="-webkit-text-stroke: 1px #dee2e6;">Contact</h1>
+                        <h1 class="position-absolute text-uppercase text-secondary">Contact Me</h1>
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8">
+                            <div class="contact-form text-center">
+                                @if (Session::has('message'))
   <div class="alert alert-secondary" role="alert">
-                                {{ Session::get('message') }}
-                              </div>
-                              <br>
+                                  {{ Session::get('message') }}
+                                </div>
+                                <br>
   @endif
-                              <form id="contactForm" method="POST" action="{{ route('contact') }}">
-                                  @csrf
-                                  <div class="form-row">
-                                      <div class="control-group col-sm-6">
-                                          <input type="text" class="form-control p-4" id="name" placeholder="Your Name"
-                                              required name="name" value="{{ old('name') }}"/>
-                                          <p class="help-block text-danger"></p>
-                                      </div>
-                                      <div class="control-group col-sm-6">
-                                          <input type="email" class="form-control p-4" id="email" placeholder="Your Email" value="{{ old('email') }}"
-                                              required name="email" />
-                                          <p class="help-block text-danger"></p>
-                                      </div>
-                                  </div>
-                                  <div class="control-group">
-                                      <input type="text" class="form-control p-4" id="subject" placeholder="Subject" value="{{ old('subject_mail') }}"
-                                          required name="subject_mail"/>
-                                      <p class="help-block text-danger"></p>
-                                  </div>
-                                  <div class="control-group">
-                                      <textarea class="form-control py-3 px-4" rows="5" id="message" placeholder="Message" name="content"
-                                        required>{{ old('content') }}</textarea>
-                                      <p class="help-block text-danger"></p>
-                                  </div>
-                                  <div>
-                                      <button class="btn btn-outline-secondary" type="submit" id="sendMessageButton">Send
-                                          Message</button>
-                                  </div>
-                                  @if ($errors->any())
+                                <form id="contactForm" method="POST" action="{{ route('contact') }}">
+                                    @csrf
+                                    <div class="form-row">
+                                        <div class="control-group col-sm-6">
+                                            <input type="text" class="form-control p-4" id="name" placeholder="Your Name"
+                                                required name="name" value="{{ old('name') }}"/>
+                                            <p class="help-block text-danger"></p>
+                                        </div>
+                                        <div class="control-group col-sm-6">
+                                            <input type="email" class="form-control p-4" id="email" placeholder="Your Email" value="{{ old('email') }}"
+                                                required name="email" />
+                                            <p class="help-block text-danger"></p>
+                                        </div>
+                                    </div>
+                                    <div class="control-group">
+                                        <input type="text" class="form-control p-4" id="subject" placeholder="Subject" value="{{ old('subject_mail') }}"
+                                            required name="subject_mail"/>
+                                        <p class="help-block text-danger"></p>
+                                    </div>
+                                    <div class="control-group">
+                                        <textarea class="form-control py-3 px-4" rows="5" id="message" placeholder="Message" name="content"
+                                          required>{{ old('content') }}</textarea>
+                                        <p class="help-block text-danger"></p>
+                                    </div>
+                                    <div>
+                                        <button class="btn btn-outline-secondary" type="submit" id="sendMessageButton">Send
+                                            Message</button>
+                                    </div>
+                                    @if ($errors->any())
   <br>
-                                  <div class="alert alert-danger">
-                                      <ul>
-                                          @foreach ($errors->all() as $error)
+                                    <div class="alert alert-danger">
+                                        <ul>
+                                            @foreach ($errors->all() as $error)
   <li>{{ $error }}</li>
   @endforeach
-                                      </ul>
-                                  </div>
+                                        </ul>
+                                    </div>
   @endif
-                              </form>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-          Contact End -->
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            Contact End -->
 
 
   <!-- Footer Start -->
